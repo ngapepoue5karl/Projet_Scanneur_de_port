70ba46315938fa82116dbbf286a8a77ac84b4393
ngapepoue5karl <ngapepouekarl@gmail.com>
2024-12-16 09:41:30 +0100

    Code_version 1.0

---
+++ Port_Scanner.py
@@ -0,0 +1,38 @@
+import socket
+import subprocess
+import sys 
+
+from datetime import datetime
+
+subprocess.call('clear', shell=True)
+
+remoteServerIP = input('Entrez l\'IP  d\'unserveur à scanner : ')
+
+print ('-' * 60)
+print (' Lancement du scan des ports de votre machine ' + remoteServerIP)
+print ('-' * 60)
+
+
+t1 = datetime.now()
+
+try:
+    for port in range (1, 1025):
+        sock = socket.socket(socket.AF_INET socket.SOCK_STREAM)
+        result = sock.connect_ex((remoteServerIP, port))
+        if result == 0:
+            print('Port {}:   Ouvert'.format(port))
+        sock.close()
+
+
+except KeyboardInterrupt:
+    print('Vous avez appuyé sur Ctrl+c pour interrompre le programme.')
+    sys.exit()
+
+except socket.error: 
+    print('Impossible de se connecter au serveur')
+    sys.exit()
+
+
+t2 = datetime.now()
+
+total = t2 -t1
